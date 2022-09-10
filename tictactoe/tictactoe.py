ea86be2d03f61d3625a35cf9aea10b2ce096b5e5
mahdi.vajhi <mvajhimv@gmail.com>
2022-09-10 12:36:07 +0430

    write player, actions, result func

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -8,6 +8,9 @@ X = "X"
 O = "O"
 EMPTY = None
 
+round = int(0)
+turn = ""
+
 
 def initial_state():
     """
@@ -22,6 +25,15 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
+    round += 1
+
+    if (round % 2) == 1:
+        turn = X
+        return X
+    if (round % 2) == 0:
+        turn = O
+        return O
+
     raise NotImplementedError
 
 
@@ -29,6 +41,15 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
+    possible_actions = set()
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+
+    return possible_actions
+
     raise NotImplementedError
 
 
@@ -36,6 +57,13 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    if not action in actions(board):
+        raise Exception("impassible action")
+
+    board[action[0]][action[1]] = turn
+
+    return board
+
     raise NotImplementedError
 
 
